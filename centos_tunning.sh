16fdd720bf13b81a672cd89c4c68442316f4b1cd
Nguyen Thanh Bien <biennt0979@gmail.com>
2023-04-17 22:15:36 +0700

    Update centos_tunning.sh

--- centos_tunning.sh
+++ centos_tunning.sh
@@ -4,9 +4,6 @@ general
 net.ipv4.ip_local_port_range = 1024 65535
 fs.file-max = 5000000
 net.core.somaxconn = 500
-net.ipv4.ip_local_port_range = 1024 65535
-fs.file-max = 5000000
-net.core.somaxconn = 500
 net.ipv4.tcp_tw_reuse = 1
 net.ipv4.tcp_timestamps = 1
 
